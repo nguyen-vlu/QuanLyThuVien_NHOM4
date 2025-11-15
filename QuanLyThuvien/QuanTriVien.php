f671602d76718f91050721a44f4163045d7b0f74
Nguyen Tai Nguyen <nguyen.2474802010261@vanlanguni.vn>
2025-11-15 21:05:14 +0700

    Update QuanTriVien.php

--- QuanLyThuvien/QuanTriVien.php
+++ QuanLyThuvien/QuanTriVien.php
@@ -27,6 +27,17 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['upload_book'])) {
     $category_id = $_POST['category_id'] ?? null;
 
     if (!$title) $errors[] = "Tiêu đề sách bắt buộc.";
+    // xử lý ảnh
+    $img_path = null;
+    if (!empty($_FILES['image']['name'])) {
+        $img = $_FILES['image'];
+        $ext = pathinfo($img['name'], PATHINFO_EXTENSION);
+        $allowed = ['jpg','jpeg','png','gif'];
+        if (!in_array(strtolower($ext), $allowed)) $errors[] = "Ảnh không hợp lệ.";
+        else {
+
+    
+
 
 
 
