045f38352a32938f69ed9a23858c06b4147d5911
ThienNguyen243 <thien.2474802010366@vanlanguni.vn>
2025-11-18 12:06:21 +0700

    Update GioiThieu.php

--- QuanLyThuvien/GioiThieu.php
+++ QuanLyThuvien/GioiThieu.php
@@ -11,22 +11,61 @@ include 'header.php';
   <li><strong>Khu tự học và học nhóm</strong>giúp sinh viên dễ dàng trao đổi kiến thức và làm việc chung.</li>
   <li><strong>Khu tra cứu điện tử</strong>với hệ thống máy tính kết nối cơ sở dữ liệu học liệu trực tuyến.</li>
 </ul>
+  
 <h2>Nguồn tài nguyên học thuật</h2>
 <ul>
   <li>Hơn 100.000 đầu sách chuyên ngành, tài liệu tham khảo và giáo trình.</li>
   <li>Nhiều tạp chí khoa học, luận văn, đề tài nghiên cứu của giảng viên và sinh viên.</li>
   <li>Thư viện số cho phép tra cứu và đọc tài liệu online mọi lúc, mọi nơi.</li>
 </ul>
-</div>
-<?php include 'footer.php';?>
-<?php session_start();?>
-<div class="header-right">
-<?php if(isset($_SESSION['user_id'])):?>
-  <span>Xin chào, <?php echo $_SESSION['ho_ten'];?></span>
-  <a href="logout.php"><button>ĐĂNG XUẤT</button></a>
-<?php else:?>
-  <a href="login.php"><button>ĐĂNG NHẬP</button></a>
-  <a href="register.php"><button>ĐĂNG KÝ</button></a>
-<?php endif; ?>
-</div>
   
+<h2>Dịch vụ hổ trợ</h2>
+<p>Thư viện cung cấp nhiều dịch vụ tiện ích như:</p>
+<ul>
+  <li>Mượn - Trả sách tự động bằng thẻ sinh viên.<li>
+  <li>Tra cứu tài liệu qua website ThuVienVLU.vn .</li>
+  <li>Hướng dẫn kỹ năng tìm kiếm thông tin và trích dẫn học thuật.</li>
+</ul> 
+  
+<h2>Sứ mệnh</h2>
+<p>Thư viện Văn Lang hướng đến việc trở thành trung tâm tri thức mở, nơi kết nối giữa con người - tri thức - công nghệ, góp phần xây dựng môi trường học tập sáng tạo, năng động và hiện đại cho cộng đồng sinh viên Văn Lang.</p> 
+</div>
+<div class = "hang1">
+  <img src = "Hinh1_ThuVienVLU.jpg" alt="">
+  <img src = "Hinh2_thuVienVLU.jpg" alt="">
+  <img src = "Hinh3_ThuVienVLU.jpg" alt="">
+</div>
+
+<div class = "hang1">
+  <img src = "Hinh4_ThuVienVLU.jpg" alt="">
+  <img src = "Hinh5_thuVienVLU.jpg" alt="">
+  <img src = "Hinh6_ThuVienVLU.jpg" alt="">
+</div>
+
+<style>
+.hang1{
+  max-width: 1000px;
+  margin: 0 auto;
+  padding: 20px;
+  display: flex;
+  gap: 16px
+  justify-content: center;
+  align-items: center;
+  flex-wrap: nowrap;
+}
+
+.hang1 img{
+  dislay: block;
+  width: 200%
+  height: 330px
+  object-fit: cover;
+  border-radius: 6px;
+}
+  
+h1{font-size:50px;}
+h2{font-size:30px;}
+p,li{font-size:20px;}
+</style>
+<?php> include 'footer.php';?>
+  
+
